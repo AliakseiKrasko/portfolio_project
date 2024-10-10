86967db4c21315fae4b48193a951dddfd3422858
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-10-10 21:06:51 +0300

    added photo section main

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import photo from './assets/images/Image.png'
+import styled from "styled-components";
 
 export const Main = () => {
     return (
@@ -6,8 +8,13 @@ export const Main = () => {
             <h1>Elias is a web designer and front-end developer</h1>
             <span>He crafts responsive websites where technologies meet creativity</span>
 
-            <img src="" alt=""/>
+            <Photo src={photo} alt="img"/>
         </div>
     );
 };
+
+const Photo = styled.img` 
+    width: 457px;
+    height: 386px;
+`
 
