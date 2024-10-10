6ae1629b0247bdf0f60a37e897daf2d05e8bdd68
AliakseiKrasko <alekskrasko1985@gmail.com>
2024-10-10 20:56:22 +0300

    added components section main

---
+++ src/layout/section/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Main = () => {
+    return (
+        <div>
+            <h1>Elias is a web designer and front-end developer</h1>
+            <span>He crafts responsive websites where technologies meet creativity</span>
+
+            <img src="" alt=""/>
+        </div>
+    );
+};
+
